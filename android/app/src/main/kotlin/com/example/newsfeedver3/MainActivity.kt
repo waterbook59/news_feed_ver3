3f9fe8b3974bc8a8b4e72ea04c847f303fb3b76e
waterbook59 <takeshi.m64@gmail.com>
2020-10-09 07:23:56 +0900

    second git font,icon,フォルダ設定

---
+++ android/app/src/main/kotlin/com/example/newsfeedver3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newsfeedver3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style.dart
@@ -0,0 +1,3 @@
+const regularFont = 'chogokubosogothic';
+const boldFont = 'SmartUI';
+
