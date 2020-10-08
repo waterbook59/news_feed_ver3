5ae1193e2b7502da0a1f2eb3d7e4dfb59ac4daa8
waterbook59 <takeshi.m64@gmail.com>
2020-10-09 08:56:13 +0900

    NewsList設定 SearchBar作成

---
+++ lib/views/news_list/components/search_bar_pat.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SearchBarPart extends StatelessWidget {
+  SearchBarPart(this.onSearch,this.errorText);
+
+  final ValueChanged<String> onSearch;
+  final TextEditingController _textEditingController = TextEditingController();
+  final String errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        maxLines: 1,
+        decoration: const InputDecoration(
+          icon: FaIcon(FontAwesomeIcons.search),
+          hintText: '検索ワードを入れてください',
+          border: InputBorder.none,
+        ),
+        controller: _textEditingController,
+        onSubmitted: onSearch,
+      ),
+    );
+  }
+}
--- lib/views/news_list/pages/news_list_page.dart
+++ lib/views/news_list/pages/news_list_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:newsfeedver3/views/news_list/components/category_chips_part.dart';
+import 'package:newsfeedver3/views/news_list/components/search_bar_pat.dart';
 
 class NewsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('news_list_page'));
+    return Scaffold(
+      body: Column(
+        children: [
+          SearchBarPart(),
+          CategoryChipsPart(),
+//          NewsItem(),
+          const CircularProgressIndicator(),
+        ],
+      ),
+    );
   }
 }
